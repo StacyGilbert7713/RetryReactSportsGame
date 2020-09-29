354455037bdbb6ab9aeab791fd1b90ebfd0855a2
Stacy Gilbert <StacyGilbert7713@gmail.com>
2020-09-28 21:10:08 -0400

    Retry Sports Game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './Components/Game/Game.js';
+import homeTeamMascot from './Assets/images/Hazelwood Husky.jpg';
+import visitingTeamMascot from './Assets/images/Parker Penguin.jpg';
 
 function App() {
+  const huskyTeam = {
+    name: 'Hazelwood Huskies',
+    logoSrc: homeTeamMascot
+  }
+
+  const penguinTeam = {
+    name: 'Parker Penguins',
+    logoSrc: visitingTeamMascot
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Hazelwood Gym"
+        homeTeam={huskyTeam}
+        visitingTeam={penguinTeam}
+      />
     </div>
   );
 }
---
+++ src/Components/Game/Game.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import Team from '../Team/Team.js';
+import ScoreBoard from '../Scoreboard/Scoreboard.js';
+import scoreSound from '../../Assets/audio/Swish+2.mp3'
+
+class Game extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      resetCount: 0,
+      homeTeamStats: {
+        shots: 0,
+        score: 0
+      },
+      visitingTeamStats: {
+        shots: 0,
+        score: 0
+      }
+    }
+
+    this.scoreSound = new Audio(scoreSound)
+  }
+
+  shoot = (team) => {
+    const teamStatsKey = `${team}TeamStats`
+    let score = this.state[teamStatsKey].score
+
+    if (Math.random() > 0.5) {
+      score += 1
+
+      setTimeout(() => {
+        this.scoreSound.play()
+      }, 100)
+    }
+
+    this.setState((state, props) => ({
+      [teamStatsKey]: {
+        shots: state[teamStatsKey].shots + 1,
+        score
+      }
+    }))
+  }
+
+  resetGame = () => {
+    this.setState((state, props) => ({
+      resetCount: state.resetCount + 1,
+      homeTeamStats: {
+        shots: 0,
+        score: 0
+      },
+      visitingTeamStats: {
+        shots: 0,
+        score: 0
+      }
+    }))
+  }
+
+  render() {
+    return (
+      <div className="Game">
+        <h1>Welcome to {this.props.venue}</h1>
+        
+        <ScoreBoard
+          visitingTeamStats={this.state.visitingTeamStats}
+          homeTeamStats={this.state.homeTeamStats}
+        />
+
+        <div className="stats">
+          <Team
+            name={this.props.homeTeam.name}
+            logo={this.props.homeTeam.logoSrc}
+            stats={this.state.homeTeamStats}
+            shotHandler={() => this.shoot('home')}
+          />
+
+          <div className="versus">
+            <h1>VS</h1>
+          </div>
+
+          <Team
+            name={this.props.visitingTeam.name}
+            logo={this.props.visitingTeam.logoSrc}
+            stats={this.state.visitingTeamStats}
+            shotHandler={() => this.shoot('visiting')}
+          />
+
+        </div>
+
+        <div className="reset">
+          <strong>Resets: </strong> {this.state.resetCount}
+          <br />
+          <button onClick={this.resetGame}>Reset Game</button>
+        </div>
+
+      </div>
+    )
+  }
+
+}
+
+export default Game;
